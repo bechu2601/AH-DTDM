16083035df467985f52cb5edc7b7a48086d09eb0
Khánh Chu <chukhanh351@gmail.com>
2024-09-04 12:39:02 +0700

    second

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ function khoiTaoMang2ChieuNgauNhien(rows, cols, minValue, maxValue) {
     for (var i = 0; i < rows; i++) {
       var row = [];
   
-      for (var j = 0; j < cols; j++) {
+        for (var j = 0; j < cols; j++) {
         // Tạo giá trị ngẫu nhiên trong khoảng [minValue, maxValue]
         var randomValue = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
         
@@ -19,5 +19,25 @@ function khoiTaoMang2ChieuNgauNhien(rows, cols, minValue, maxValue) {
   }
 
 
-var res  = khoiTaoMang2ChieuNgauNhien(3, 3, 0, 30);
+var res  = khoiTaoMang2ChieuNgauNhien(3, 4, 10, 99);
 console.log(res);
+
+
+const resMang1 = res.map(row => row.map(value => value * 2));
+console.log(resMang1)
+
+
+function khoiTaoMang1ChieuNgauNhien(rows, minValue, maxValue){
+  var row = []
+  for(i = 0; i < rows; i++){
+    var randomValue = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+    row.push(randomValue);
+  }
+  return row;
+}
+
+var kq = khoiTaoMang1ChieuNgauNhien(5, 1, 10);
+console.log(kq);
+
+var kqMang = kq.map(kq => kq * kq);
+console.log(kqMang);
